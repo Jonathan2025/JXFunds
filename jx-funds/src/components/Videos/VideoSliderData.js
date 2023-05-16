731e13eaa09bedad2fceb2b423cd87215e75368b
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-16 18:25:29 -0400

    added video 2

--- jx-funds/src/components/Videos/VideoSliderData.js
+++ jx-funds/src/components/Videos/VideoSliderData.js
@@ -5,10 +5,10 @@ export const VideoSliderData = [
         heading: "1 - New to Investing? Start Here!",
         desc: "Jonathan teaches you what investing is and other financial concepts in an easy to understand way!", 
     },
-    // {   
-    //     image: "https://www.valuewalk.com/wp-content/uploads/2020/02/value_investing_1582184733.jpg",
-    //     // video: <video src={require('./Jonathans_Tomagotchi_Project.mp4')} width="560" height="315" controls></video>,
-    //     heading: "New to Investing? Start Here!",
-    //     desc: "Jonathan teaches you the best roadmap for investing", 
-    // }
+    {   
+        image: "https://cdn.corporatefinanceinstitute.com/assets/income-investing-1024x576.jpeg",
+        video:"https://jxfundsvideosaccount.blob.core.windows.net/jxfundsnewcontainer/video2.mp4",
+        heading: "How to use JX Funds App",
+        desc: "In this short video, we demonstrate ways you can use our app for your own purposes. We hope this app starts you on the path to happy and wealthy future!", 
+    }
 ]
