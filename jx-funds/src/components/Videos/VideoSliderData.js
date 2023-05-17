ea96985945ce0a5f0de54bd75d4dada10a58e3cf
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-17 16:17:37 -0400

    added another video to the app

--- jx-funds/src/components/Videos/VideoSliderData.js
+++ jx-funds/src/components/Videos/VideoSliderData.js
@@ -6,6 +6,12 @@ export const VideoSliderData = [
         desc: "Jonathan teaches you what investing is and other financial concepts in an easy to understand way!", 
     },
     {   
+        image: "https://www.isgfunds.com.au/wp-content/uploads/2022/01/Your-best-investment-blog.png",
+        video: "https://jxfundsvideosaccount.blob.core.windows.net/jxfundsnewcontainer/Video3.mp4",
+        heading: "2 - Different Types of Investment Accounts",
+        desc: "Learn how to get started with investing and gain a broad overview of the different investment accounts", 
+    },
+    {   
         image: "https://cdn.corporatefinanceinstitute.com/assets/income-investing-1024x576.jpeg",
         video:"https://jxfundsvideosaccount.blob.core.windows.net/jxfundsnewcontainer/video2.mp4",
         heading: "How to use JX Funds App",
